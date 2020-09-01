77963047e1880bf053e7e2cd410c977dbbfd6d08
Yaro <ysboyko@gmail.com>
2020-08-31 20:06:12 -0400

    Updated Profile Page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonItem, IonInput, IonLabel } from '@ionic/react';
+import React, { useState, useCallback, useContext } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonItem, IonInput, IonLabel, IonRouterLink } from '@ionic/react';
+import { auth } from 'firebase';
+import { UserContext } from '..';
+import firebase from 'firebase';
 
 const Profile: React.FC = () => {
 
   const [text, setText] = useState<string>();
-  
+  const user = useContext(UserContext);
+
+  const signOut = useCallback(() => {
+    firebase.auth().signOut();
+  }, []);
+
+  const email = user.email;
+  const name = user.displayName;
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,14 +23,20 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>        
-        <p>Add stats here:
-          Email<br />
-          Profile Pic?<br />
-          How many Surveys finished<br />
-          How many Surveys Completed?<br />
-          Etc<br />
-        </p>
+      <IonContent>
+        <IonCard>
+          <IonCardContent>
+          <IonItem>
+              <IonLabel position="fixed">Name</IonLabel>
+              { name }
+            </IonItem>
+            <IonItem>
+              <IonLabel position="fixed">Email</IonLabel>
+              { email }
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
+        <IonButton class="ion-padding" expand="block" onClick={signOut} routerLink="/Home">Sign Out</IonButton>
       </IonContent>
     </IonPage>
   );
